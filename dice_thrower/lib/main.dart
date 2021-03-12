1d04d8c2ea358d1f47d368ba7ce2cc20eb99eedf
mehrbarz <mehrbarzegar@gmail.com>
2021-03-12 18:15:42 +0100

    variable instead of hardcoded number

--- dice_thrower/lib/main.dart
+++ dice_thrower/lib/main.dart
@@ -1,33 +1,44 @@
 import 'package:flutter/material.dart';
 
 void main() {
-    return runApp(
-    MaterialApp(
-      debugShowCheckedModeBanner: false,
-      home: Scaffold(
-        backgroundColor: Colors.cyan,
-        appBar: AppBar(
-          title: Text('Dice thrower'),
-          backgroundColor: Colors.lime,
-        ),
-        body: DicePage(),
+  return runApp(MaterialApp(
+    debugShowCheckedModeBanner: false,
+    home: Scaffold(
+      backgroundColor: Colors.cyan,
+      appBar: AppBar(
+        title: Text('Dice thrower'),
+        backgroundColor: Colors.lime,
       ),
-    )
-  );
+      body: DicePage(),
+    ),
+  ));
 }
 
 class DicePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Row(
-      children: [
-        Expanded(child: Image.asset('images/dice1.png')
-        ),
-        Expanded(child: Image.asset('images/dice1.png')
-        ),
-      ],
-
+    var leftDiceNumber = 2 ;
+    return Center(
+      child: Row(
+        children: [
+          Expanded(
+            child: FlatButton(
+              onPressed: () {
+                print('Left button pressed');
+              },
+              child: Image.asset('images/dice$leftDiceNumber.png'),
+            ),
+          ),
+          Expanded(
+            child: FlatButton(
+              onPressed: () {
+                print('Right button is pressed');
+              },
+              child: Image.asset('images/dice3.png'),
+            ),
+          ),
+        ],
+      ),
     );
   }
 }
-
