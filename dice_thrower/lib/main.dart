31e5bf6d336e75778ae918984f93961a14c87420
mehrbarz <mehrbarzegar@gmail.com>
2021-03-13 11:28:49 +0100

    Stateful - math - Random - nextInt()

--- dice_thrower/lib/main.dart
+++ dice_thrower/lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'dart:math';
 
 void main() {
   return runApp(MaterialApp(
@@ -14,17 +15,26 @@ void main() {
   ));
 }
 
-class DicePage extends StatelessWidget {
+class DicePage extends StatefulWidget {
+  @override
+  _DicePageState createState() => _DicePageState();
+}
+
+class _DicePageState extends State<DicePage> {
+  int leftDiceNumber = 2 ;
   @override
   Widget build(BuildContext context) {
-    var leftDiceNumber = 2 ;
+
     return Center(
       child: Row(
         children: [
           Expanded(
             child: FlatButton(
               onPressed: () {
-                print('Left button pressed');
+                setState(() {
+                  leftDiceNumber= Random().nextInt(6)+1;
+                  print('diceNumber = $leftDiceNumber');
+                });
               },
               child: Image.asset('images/dice$leftDiceNumber.png'),
             ),
@@ -42,3 +52,5 @@ class DicePage extends StatelessWidget {
     );
   }
 }
+
+
